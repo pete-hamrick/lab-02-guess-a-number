c73d46c44007a35c5a3656329c49cce879afed58
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-30 13:24:41 -0700

    created basic html with IDs

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // import functions and grab DOM elements
 
+// const userGuess = document.getElementById('user-guess');
+// const guessBtn = document.getElementById('guess-button');
+
 // initialize state
+// const guessesRemaining = 4;
+// const targetNumber = Math.floor(Math.random() * 20);
 
 // set event listeners 
   // get user input
