942f46bce68f90169cf478db2cf0e6039e19a2c2
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-30 14:44:31 -0700

    referenced DOM elements and initialzed state in app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // import functions and grab DOM elements
 
-// const userGuess = document.getElementById('user-guess');
-// const guessBtn = document.getElementById('guess-button');
+const guessBtn = document.getElementById('guess-btn');
 
 // initialize state
-// const guessesRemaining = 4;
-// const targetNumber = Math.floor(Math.random() * 20);
+const userGuess = document.getElementById('user-guess');
+let guessesRemaining = 4;
+const targetNumber = Math.floor(Math.random() * 20);
 
 // set event listeners 
+guessBtn.addEventListener('click', ()=> {
+    console.log(userGuess, guessesRemaining, targetNumber);
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
--- test/example.test.js
+++ test/example.test.js
@@ -3,7 +3,35 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('userGuess is too high', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 8;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('userGuess is too low', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('userGuess is right on', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
