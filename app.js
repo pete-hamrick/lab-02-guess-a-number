80678465ce75e9ee7b26078a39edf606f704ed5c
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-30 16:09:27 -0700

    partial function written

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // import functions and grab DOM elements
 import { compareNumbers } from './utils.js';
 const guessBtn = document.getElementById('guess-btn');
+const feedback = document.getElementById('feedback');
 
 // initialize state
 const userGuess = document.getElementById('user-guess');
-let guessesRemaining = 4;
+let guessesRemaining = 3;
 const targetNumber = Math.floor(Math.random() * 20);
 
 // set event listeners 
 guessBtn.addEventListener('click', ()=> {
+  //          no tries remaining?
+    if (guessesRemaining === 0) {
+//                  display lose message
+        feedback.textContent = 'You are out of guesses! Game over.';
+//                  disable game play
+
+    }
+        //      decrement tries remaining  
     console.log(userGuess, guessesRemaining, targetNumber, userGuessNum, compareNumbers);
     const userGuessNum = Number(userGuess);
+    guessesRemaining--;
      
 });
   // get user input
--- utils.js
+++ utils.js
@@ -1,3 +1,21 @@
-export function compareNumbers(targetNumber, userGuess){
-    console.log(targetNumber, userGuess);
+export function compareNumbers(targetNumber, userGuessNum){
+    let result;
+    // if guess is correct
+    if (targetNumber === userGuessNum) {
+        //      display YOU GOT IT
+        result.textContent = 'You Win! Game Over!';
+        //      disable game play
+
+        // else if guess is too high
+    } else if (targetNumber > userGuessNum) {
+
+        //      display your guess is too high
+        result.textContent = 'Your guess is too high';
+        // else if guess is too low
+    } else {
+        result.textContent = 'Your guess is too low';
+    }
+    //      display your guess is too low
+    //      
+    //          
 }
