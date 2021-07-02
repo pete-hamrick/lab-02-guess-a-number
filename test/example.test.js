36402179e93417419c17141b291b8ba0d2359ecd
Evon Perez <perez.evon@gmail.com>
2021-07-02 17:07:56 -0400

    wrote test functions in test fil

--- test/example.test.js
+++ test/example.test.js
@@ -6,7 +6,7 @@ const test = QUnit.test;
 test('userGuess is too high', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = 1;
+    const expected = 'too high';
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -16,6 +16,36 @@ test('userGuess is too high', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('userGuess is too low', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'too low';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(2, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('userGuess is correct', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'correct';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(1, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
 
 
 
--- utils.js
+++ utils.js
@@ -1,6 +1,15 @@
 export 
 
 
+// compareNumbers, will compare user target number(int) against guess(int)
+//user trying to guess number, checking if user guessed correct or high or low
+//spits out somthing (return) -> string
+//if too high -> tell user guess too high
+//if too low -> tell user too low
+//if user guess equals target number -> winning mesg and stop game
+
+
+
 
 
 
